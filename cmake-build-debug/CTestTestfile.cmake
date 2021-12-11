383efa998fbfa9f4868dbf33489f941ef833ba2d
premekb <38076249+premekb@users.noreply.github.com>
2021-12-11 04:42:39 +0100

    untrack cmake debug dir

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/premekbelka/Documents/Semestr 3/PJC/semestralka/pjc-semestralka
-# Build directory: /Users/premekbelka/Documents/Semestr 3/PJC/semestralka/pjc-semestralka/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[tests]=] "/Users/premekbelka/Documents/Semestr 3/PJC/semestralka/pjc-semestralka/cmake-build-debug/tests")
-set_tests_properties([=[tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/premekbelka/Documents/Semestr 3/PJC/semestralka/pjc-semestralka/CMakeLists.txt;11;add_test;/Users/premekbelka/Documents/Semestr 3/PJC/semestralka/pjc-semestralka/CMakeLists.txt;0;")
